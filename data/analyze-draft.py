0d77a1128b55b63ef89dc882b122d75447dd31fd
Quynh Khanh Nguyen <87851289+khanh-quynh@users.noreply.github.com>
2023-09-25 01:15:21 -0400

    Update analyze-draft.py

--- data/analyze-draft.py
+++ data/analyze-draft.py
@@ -15,7 +15,7 @@ def main():
                 year_count+=1
                 avg_anomly_year = (float(line['Jan']) + float(line['Feb']) + float(line['Mar']) + float(line['Apr']) + float(line['May']) + float(line['Jun']) + float(line['Jul']) + float(line['Aug']) + float(line['Sep']) + float(line['Oct']) + float(line['Nov']) + float(line['Dec']))/12
                 sum_anomly_decade += avg_anomly_year
-        result = round(sum_anomly_decade/10, 2)
+        result = round(sum_anomly_decade/year_count, 2)
         
         print(str(current_decade) + "-" + str(current_decade+year_count-1).ljust(10) +  str(result).rjust(5))
 
